6ad8c64df7c24d2ccd9130914d93fc4fe41f2f16
asdasda123 <asdasdad@gmail.com>
2022-12-12 20:20:05 +0700

    minggu 11 rev 2

--- minggu11/algorithm.js
+++ minggu11/algorithm.js
@@ -30,19 +30,43 @@ var anggota = ['Toni', 'Joko', 'Andi'];
 var data = document.anggota,
     totalAnggota = document.getElementById("jumlahMahasiswa"),
     tabelAnggota = document.getElementById("dataHMTI");
+
+var addData = function () {
+    var result = ``;
+    if (anggota.length > 0) {
+        for (var i = 0; i < anggota.length; i++) {
+            result +=  `<tr>`;
+            result +=  `<td>Nama : </td>
+                        <td>${anggota[i]}</td>
+                        <td><input type="button" onclick="_editData(${i})" value="Edit"></td>
+                        <td><input type="button" onclick="_deleteData(${i})" value="Delete"></td>`;
+            result +=  `</tr>`;
+        }
+    }
+    return tabelAnggota.innerHTML = result;
+}
+
+var _editData = function (value) {
+    let confirmEdit = confirm("Apakah anda yakin ingin mengedit data ini?");
+    if (confirmEdit) {
+        let newData = prompt("Edit data");
+        if (newData != null) {
+            anggota[value] = newData;
+            addData();
+        }
+    }
+}
+
+var _deleteData = function (value) {
+    let confirmDelete = confirm("Apakah anda yakin ingin menghapus data ini?");
+    if (confirmDelete) {
+        anggota.splice(value, 1);
+        addData();
+    }
+}
+
 var _submitUser = function () {
     this.anggota.push(data.nama.value);
     this.totalAnggota.innerHTML = `Jumlah anggota HMTI 2023 adalah ${anggota.length} Mahasiswa`;
-    this.addData = function () {
-        var result = ``;
-        if (anggota.length > 0) {
-            for (var i = 0; i < anggota.length; i++) {
-                result += `<tr>`;
-                result += `<td>Nama : </td><td>${anggota[i]}</td>`;
-                result += `</tr>`;
-            }
-        }
-        return tabelAnggota.innerHTML = result;
-    }
+    addData();
 }
-_submitUser.addData();
