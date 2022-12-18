40be8df4c0c4990a5ec248c1178e79dc0026479e
asdasda123 <asdasdad@gmail.com>
2022-12-19 02:51:32 +0700

    hosting tugas akhir

--- tugas_akhir/config/config.js
+++ tugas_akhir/config/config.js
@@ -2,3 +2,8 @@ var width = window.innerWidth;
 var height = window.innerHeight;
 
 console.log(`The width is ${width} and the height is ${height}`);
+
+var guestName = function () {
+    const name = document.getElementById('guestName').innerHTML;
+    return name;
+}
---
+++ tugas_akhir/content/config/config.js
@@ -0,0 +1,4 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+console.log(`The width is ${width} and the height is ${height}`);
